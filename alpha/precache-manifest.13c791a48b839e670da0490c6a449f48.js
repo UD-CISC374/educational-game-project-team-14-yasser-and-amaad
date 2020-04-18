3f9513c088bba0f23a9d0cab9ec03234bf32128f
Yasser Abdelaal <ayasser@udel.edu>
2020-04-17 23:57:11 -0400

    Updates

---
+++ alpha/precache-manifest.13c791a48b839e670da0490c6a449f48.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4424eed8071babaebf81e310118462b",
+    "url": "/lmntor/alphaassets/Map1.json"
+  },
+  {
+    "revision": "32252d504fa0470cb634a360137ef74a",
+    "url": "/lmntor/alphaassets/Tileset 1.png"
+  },
+  {
+    "revision": "1fce4b0c68c05459fe06454ba922c161",
+    "url": "/lmntor/alphaassets/back.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/lmntor/alphaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/lmntor/alphaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/lmntor/alphaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/lmntor/alphaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/lmntor/alphaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/lmntor/alphaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "f271b57563082ca2c908c69e4e2c5c01",
+    "url": "/lmntor/alphaassets/tmp/background.png"
+  },
+  {
+    "revision": "c27b836f0e4f622cf69e8ab82904ee97",
+    "url": "/lmntor/alphaassets/tmp/book.png"
+  },
+  {
+    "revision": "d48ef6b8f3ca981f9267df5afc250093",
+    "url": "/lmntor/alphaassets/tmp/hydrogen.png"
+  },
+  {
+    "revision": "3f8b5eb2a965d26fa7f08a7f65e04603",
+    "url": "/lmntor/alphaassets/tmp/kenney_player.png"
+  },
+  {
+    "revision": "8607b990a74b1012d97e88c53fc05b3f",
+    "url": "/lmntor/alphaassets/tmp/kenney_player_atlas.json"
+  },
+  {
+    "revision": "bf077a6aa81a460ede4682fb8914bcc5",
+    "url": "/lmntor/alphaassets/tmp/map2.json"
+  },
+  {
+    "revision": "83670f9befcd4281553e7aedb194b877",
+    "url": "/lmntor/alphaassets/tmp/map2.tmx"
+  },
+  {
+    "revision": "548d362183ad41bba6624b086f21611b",
+    "url": "/lmntor/alphaassets/tmp/oxygen.png"
+  },
+  {
+    "revision": "efe5235a08232dbc4ec83b69f5654a41",
+    "url": "/lmntor/alphaassets/tmp/platformPack_tilesheet.png"
+  },
+  {
+    "revision": "10ff1bbee3241545b6c47055a92ec145",
+    "url": "/lmntor/alphaassets/tmp/spike.png"
+  },
+  {
+    "revision": "af5be204ef641c6e546d23de204901c5",
+    "url": "/lmntor/alphaassets/tmp/wizard sprite.png"
+  },
+  {
+    "revision": "9c88305524efbd48992eb62cc033ff76",
+    "url": "/lmntor/alphaassets/tmp/wizard walk.json"
+  },
+  {
+    "revision": "e74fd4983c694d5cabf668629db0b2bc",
+    "url": "/lmntor/alphaassets/tmp/wizard walk.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/lmntor/alphafavicon.ico"
+  },
+  {
+    "revision": "4b24dd3d15baac956582",
+    "url": "/lmntor/alphagen.main.681abf6a60a7992802e1.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/lmntor/alphagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "e4bcc5f4c503794956496970f6b464ef",
+    "url": "/lmntor/alphaindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/lmntor/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/lmntor/alphasw.js"
+  }
+]);

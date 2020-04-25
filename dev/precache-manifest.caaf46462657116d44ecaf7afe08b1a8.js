848d7e3fd4b9c16e1281ac88dc57978da82eb986
Amaad Rafi <amaadrafi@gmail.com>
2020-04-24 22:28:36 -0400

    Updates

--- dev/precache-manifest.caaf46462657116d44ecaf7afe08b1a8.js
+++ dev/precache-manifest.caaf46462657116d44ecaf7afe08b1a8.js
@@ -1,5 +1,49 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
+    "revision": "1b37d71e6613877ee1bca1e157f22067",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/Attack1.png"
+  },
+  {
+    "revision": "9a3201b9bd1a03679287ba60a23494c1",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/Attack2.png"
+  },
+  {
+    "revision": "5f7683d03534f5b66116f6b84c2ce02f",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/Death.png"
+  },
+  {
+    "revision": "510e20702fb41b8becac4952ee97fe67",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/Fall.png"
+  },
+  {
+    "revision": "53981dafa29fb690237cd788320b24f7",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/Hit.png"
+  },
+  {
+    "revision": "6df18543cb030d749707737d1dad3599",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/Idle.png"
+  },
+  {
+    "revision": "4bca07882620ce34d7fa811a8a3aa463",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/Jump.png"
+  },
+  {
+    "revision": "baa5be88f044a0c8b1795393ad2ac382",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/Run.png"
+  },
+  {
+    "revision": "bb418a0839c41bb49214360e83757bd9",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/idle.json"
+  },
+  {
+    "revision": "f27401328529010032906856869d3651",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/jump.json"
+  },
+  {
+    "revision": "d27271cda1509c848f3864233c59fa1a",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/character/run.json"
+  },
+  {
     "revision": "e7087d5d91321d575b038366ef9f2407",
     "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/citations.md"
   },
@@ -22,6 +66,30 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "200ce541edfaa71be93a1041736d1fee",
     "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "46e5f704bd52b0502f45f9ecf34fc456",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/level_1/L1.json"
+  },
+  {
+    "revision": "0235b3d1a302d5d65c6641317832fd8e",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/level_1/Level_1.json"
+  },
+  {
+    "revision": "b5788327ae3bf9d3258b2b8c1b09a1a4",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/level_1/Level_12.json"
+  },
+  {
+    "revision": "7bc7195d820c3a4f5d4a0cc85abee0a5",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/level_1/rocks.png"
+  },
+  {
+    "revision": "f56e40e2dccc97b973304418b75f2d98",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/level_1/sign.png"
+  },
+  {
+    "revision": "bad8c15bd025488bf951483940774e62",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devassets/level_1/tiles_spritesheet.png"
   },
   {
     "revision": "f271b57563082ca2c908c69e4e2c5c01",
@@ -64,15 +132,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/educational-game-project-team-14-yasser-and-amaad/devfavicon.ico"
   },
   {
-    "revision": "9f3e56a9e75488de800d",
-    "url": "/educational-game-project-team-14-yasser-and-amaad/devgen.main.a9c0aeaee6b886c84024.bundle.js"
+    "revision": "a305ce85a9ae0f440467",
+    "url": "/educational-game-project-team-14-yasser-and-amaad/devgen.main.2691fb4522164913080f.bundle.js"
   },
   {
     "revision": "5e69403aa42e950fedff",
     "url": "/educational-game-project-team-14-yasser-and-amaad/devgen.vendors.0805bb80903235d40eaf.bundle.js"
   },
   {
-    "revision": "3abca647a444039d18da7ff96094f8c3",
+    "revision": "49784cdcf209606cf9f62e1cadf09bee",
     "url": "/educational-game-project-team-14-yasser-and-amaad/devindex.html"
   },
   {
